b525f42bf735480720bcb8f80f0dd45864f03784
Thomas <tomas.zach.tz@gmail.com>
2025-03-10 18:00:40 +0100

    v1.8

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import { filterProductsByCategory } from "./moduls/product-filter.js";
 import { fetchProduct, dataProduct } from "./moduls/fetch-product.js";
 
-// Použij Promise a then() v index.js
 fetchProduct().then(() => {
-    console.log(dataProduct);  // Data budou k dispozici až po načtení
+    console.log(dataProduct); 
     filterProductsByCategory('Novinky')
 });
 
 document.querySelectorAll('.filter-btn').forEach(button => {
     button.addEventListener('click', () => {
-        // console.log('1')
-        const category = button.getAttribute('data-category'); 
-        console.log(category)
-        filterProductsByCategory(category); 
+
+        document.querySelectorAll('.filter-btn').forEach(btn => {
+            btn.classList.remove('active');
+        });
+
+
+        button.classList.add('active');
+
+        const category = button.getAttribute('data-category');
+        console.log(category);
+        filterProductsByCategory(category);
     });
 });
+
 
 // addon pro px obrazovky
 window.addEventListener('resize', () => {
--- moduls/product-filter.js
+++ moduls/product-filter.js
@@ -1,12 +1,12 @@
 import { dataProduct } from './fetch-product.js';
 let firstLoad = true;
 export async function filterProductsByCategory(category) {
-    console.log('sss')
+    //console.log('sss')
     let indexHTML = '';
     const productsContainer = document.querySelector('.product-content');
     if (dataProduct && firstLoad) {
         const filteredProducts = dataProduct.filter(product => product.category === category);
-        console.log('3 itemy')
+        //console.log('3 itemy')
         //console.log(filteredProducts); 
         let firstLoadCont = [];
         firstLoadCont = filteredProducts.slice(0,4)
@@ -15,7 +15,7 @@ export async function filterProductsByCategory(category) {
             indexHTML += `
                 <article class="product">
                     <figure>
-                        <img src='${product.imgSrc}'>
+                        <img class="img-product" src='${product.imgSrc}'>
                     </figure>
                         <a href="">${product.title}</a>
                         <h2>${product.availability}</h2>
@@ -37,11 +37,11 @@ export async function filterProductsByCategory(category) {
     else if (!firstLoad && category === 'all-product') {
         console.log('all product funguje') 
      
-        dataProduct.forEach(product => {
+        data.forEach(product => {
             indexHTML += `
                 <article class="product">
                     <figure>
-                        <img src='${product.imgSrc}'>
+                        <img class="img-product"src='${product.imgSrc}'>
                     </figure>
                         <a href="">${product.title}</a>
                         <h2>${product.availability}</h2>
@@ -63,14 +63,14 @@ export async function filterProductsByCategory(category) {
     }
      
     else if (!firstLoad && category){
-        console.log("max itemy")
+        //console.log("max itemy")
         const filteredProducts = dataProduct.filter(product => product.category === category);
         //console.log(filteredProducts);    
         filteredProducts.forEach(product => {
             indexHTML += `
                 <article class="product">
                     <figure>
-                        <img src='${product.imgSrc}'>
+                        <img class="img-product" src='${product.imgSrc}'>
                     </figure>
                         <a href="">${product.title}</a>
                         <h2>${product.availability}</h2>
